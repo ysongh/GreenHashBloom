59c0353b9ead0e5ef55eb8d503bf8b367ebb8cf3
You Song Hou <ysongweb3@gmail.com>
2025-11-16 00:29:30 -0500

    chore: add Hedera testnet network to hardhat.config.js

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -34,5 +34,10 @@ export default defineConfig({
       url: configVariable("SEPOLIA_RPC_URL"),
       accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
     },
+    hederatestnet: {
+      type: "http",
+      url: "https://testnet.hashio.io/api",
+      accounts: [configVariable("HEDERA_PRIVATE_KEY")],
+    },
   },
 });
